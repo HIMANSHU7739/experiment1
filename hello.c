d06462f21d468186f4a5f9b9548568adba480d83
HIMANSHU7739 <himanshu30506@gmail.com>
2025-09-08 14:56:46 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    printf("himanshu singh\n");
+    printf("sector-6\n");
+    printf("jagriti vihar\n");
+    printf("meerut")
+    printf;("uttar pradesh\n");
+    printf("pincode-250001\n");
+    return 0;
+}
